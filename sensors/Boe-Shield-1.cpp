7713a8cd09fcbbaf6817c0afca44cdf48e3bfbf3
xxthexxscenexx <whyarntrobotsincontrolofearth@gmail.com>
2014-12-27 21:14:41 -0500

    Update Boe-Shield-1.cpp

--- sensors/Boe-Shield-1.cpp
+++ sensors/Boe-Shield-1.cpp
@@ -22,12 +22,16 @@ void loop()
   // The ultrasonic sensor is triggered by a HIGH pulse of 2 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, LOW);
   
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(1);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(trigPin, LOW);
+
+  // The same pin is used to read the signal from the ultrasonic sensor: a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
 
@@ -36,28 +40,32 @@ void loop()
   cm = microsecondsToCentimeters(duration);
   
   // conditional to seek distance 
-  if (inches > 5)
+  if (inches > 10)
   {
     forward();
   }  
-  else if (inches < 5) 
+  else if (inches < 10) 
   {
     turnLeft();
+    turnLeft();
   }
-  
 }
 
 long microsecondsToInches(long microseconds)
 {
-  // 73.746 microseconds per inch 
-  // divide by 2 to get the distance of the obstacle
+  // According to Parallax's datasheet for the sensor, there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
 }
 
 long microsecondsToCentimeters(long microseconds)
 {
-  // speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // to find the distance of the take half of the distance travelled.
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
   return microseconds / 29 / 2;
 }
 
@@ -73,4 +81,3 @@ void turnLeft() {
   servoLeft.write(0);
   servoRight.write(0);
 }
-
